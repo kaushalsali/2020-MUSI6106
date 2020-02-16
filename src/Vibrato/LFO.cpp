bd7c936e00facfd6ac4f7ce6c5421c534beffc5e
Kaushal Sali <salikaushal@gmail.com>
2020-02-16 18:39:34 -0500

    WaveTableOscillator implementation added.

--- src/Vibrato/LFO.cpp
+++ src/Vibrato/LFO.cpp
@@ -2,3 +2,73 @@
 // Created by Kaushal Sali on 16/02/20.
 //
 
+//
+// Created by Kaushal Sali on 15/02/20.
+//
+
+#include "LFO.h"
+#include "Synthesis.h"
+
+template<class T>
+WaveTableOscillator<T>::WaveTableOscillator(int waveTableSize) :
+        m_waveTableSize(waveTableSize),
+        m_pWaveTable(new CRingBuffer<T> (waveTableSize))
+{
+}
+
+
+template<class T>
+T WaveTableOscillator<T>::getNextSample() {
+    auto sample = m_pWaveTable->get(m_currentSample);
+    m_currentSample += m_sampleDelta % m_waveTableSize;
+    return sample;
+}
+
+template<class T>
+Error_t WaveTableOscillator<T>::setWavefromType(Waveform waveformType) {
+    m_waveformType = waveformType;
+    return kNoError;
+}
+
+template<class T>
+Error_t WaveTableOscillator<T>::setFrequency(float frequencyInHz) {
+    m_frequency = frequencyInHz;
+    return kNoError;
+}
+
+template<class T>
+Error_t WaveTableOscillator<T>::setSampleRate(int sampleRateInHz) {
+    m_sampleRate = sampleRateInHz;
+    return kNoError;
+}
+
+template<class T>
+Error_t WaveTableOscillator<T>::updateWaveTable() {
+    auto cyclesPerSample = m_frequency / m_sampleRate;
+    m_sampleDelta = m_waveTableSize * cyclesPerSample; // sample resolution //TODO: Check
+
+    // Generate waveform and fill WaveTable
+    auto* waveform = new float[m_waveTableSize];
+    auto waveformSampleRate = m_waveTableSize * m_frequency; //for sampling the waveform such that wavetable size equal one period.
+    switch (m_waveformType) {
+        case Waveform::Sine:
+            CSynthesis::generateSine(waveform, waveformSampleRate, m_waveTableSize);
+            break;
+        case Waveform::Rectangle:
+            CSynthesis::generateRect(waveform, waveformSampleRate, m_waveTableSize);
+            break;
+        case Waveform::Saw:
+            CSynthesis::generateSaw(waveform, waveformSampleRate, m_waveTableSize);
+            break;
+    }
+    m_pWaveTable->put(waveform, m_waveTableSize);
+    return kNoError;
+}
+
+template<class T>
+Error_t WaveTableOscillator<T>::init(WaveTableOscillator::Waveform waveformType, float frequencyInHz, int sampleRateInHz) {
+    setWavefrom(waveformType);
+    setFrequency(frequencyInHz);
+    setSampleRate(sampleRateInHz);
+    updateWaveTable();
+}
--- src/inc/LFO.h
+++ src/inc/LFO.h
@@ -1,8 +1,68 @@
 //
-// Created by Kaushal Sali on 16/02/20.
+// Created by Kaushal Sali on 15/02/20.
 //
 
 #ifndef MUSI6106_LFO_H
 #define MUSI6106_LFO_H
+
+#include "ErrorDef.h"
+#include "RingBuffer.h"
+#include "Synthesis.h"
+
+template <class T>
+
+class LFO {
+public:
+    enum Waveform
+    {
+        Sine,
+        Rectangle,
+        Saw,
+        DC
+    };
+
+    LFO (Waveform wavetype, float freqInHz, int sampleRateInHz);
+
+    Error_t setWavefrom();
+    Error_t setFrequency(float frequencyInHz, int sampleRateInHz);
+    T getNextSample();
+
+private:
+    LFO();
+
+
+};
+
+
+template <class T>
+class WaveTableOscillator {
+public:
+    enum Waveform
+    {
+        Sine,
+        Rectangle,
+        Saw,
+    };
+
+    explicit WaveTableOscillator (int waveTableSize);
+    Error_t setWavefromType(Waveform waveformType);
+    Error_t setFrequency(float frequencyInHz);
+    Error_t setSampleRate(int sampleRate);
+    Error_t updateWaveTable();
+    Error_t init(Waveform waveformType, float frequencyInHz, int sampleRateInHz);
+
+    T getNextSample();
+
+private:
+    int m_waveTableSize;
+    CRingBuffer<T>* m_pWaveTable;
+    Waveform m_waveformType;
+    int m_frequency;
+    int m_sampleRate;
+    float m_currentSample = 0.0f;
+    float m_sampleDelta = 0.0f;
+};
+
+
 
 #endif //MUSI6106_LFO_H
